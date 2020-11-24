df0a90f4f6c1e9c3cf6fcbac1111b0b52b09f2c0
rsleyland <71075940+rsleyland@users.noreply.github.com>
2020-11-24 10:41:34 -0500

    Update main.rb

--- Ryan/Q2/main.rb
+++ Ryan/Q2/main.rb
@@ -8,27 +8,29 @@ while line = f.gets
   line_splits = line.split(/\s/)
 
   case line_splits[0]
+  when nil
+    puts 'EMPTY LINE!'
   when 'shape'
     Shape.new.print
   when 'circle'
     if line_splits[1].to_i.negative?
-      puts 'Invalid radius'
+      puts 'Error: Invalid Circle - radius < 0'
     else
       Circle.new(line_splits[1].to_i).print
     end
   when 'rectangle'
     if line_splits[1].to_i.negative?
-      puts 'Invalid Height'
+      puts 'Error: Invalid Rectangle - Height < 0'
     elsif line_splits[2].to_i.negative?
-      puts 'Invalid Width'
+      puts 'Error: Invalid Rectangle - Width < 0'
     else
       Rectangle.new(line_splits[1].to_i, line_splits[2].to_i).print
     end
   when 'ellipse'
     if line_splits[1].to_i.negative?
-      puts 'Invalid Height'
+      puts 'Error: Invalid Ellipse - Semi-Major < 0'
     elsif line_splits[2].to_i.negative?
-      puts 'Invalid Width'
+      puts 'Error: Invalid Ellipse - Semi-Minor < 0'
     else
       Ellipse.new(line_splits[1].to_i, line_splits[2].to_i).print
     end
@@ -36,7 +38,3 @@ while line = f.gets
     puts 'NOT A SHAPE!'
   end
 end
-
-
-
-
